66c39f5e7488dc94cd83a9aeb07b4aba57e2aff3
alishouaa <alishouaa32@hotmail.com>
2021-04-10 20:25:21 +0300

    first commit

---
+++ js/style.js
@@ -0,0 +1,103 @@
+$(window).scroll(function() {
+
+    if ($(this).scrollTop() >= 200) {
+        $(".nav").addClass("noTransparrent");
+    } else {
+        $(".nav").removeClass("noTransparrent");
+    }
+
+});
+
+
+
+function btn1() {
+    document.querySelector(".img1").style.opacity = "1.5";
+    document.querySelector(".img2").style.opacity = "1.5";
+    document.querySelector(".img4").style.opacity = "1.5";
+    document.querySelector(".img5").style.opacity = "1.5";
+    document.querySelector(".img3").style.opacity = "1.5";
+    document.querySelector(".img6").style.opacity = "1.5";
+    /* *************************************************************/
+    document.querySelector(".img1").style.padding = "0px";
+    document.querySelector(".img2").style.padding = "0px";
+    document.querySelector(".img4").style.padding = "0px";
+    document.querySelector(".img5").style.padding = "0px";
+    document.querySelector(".img3").style.padding = "0";
+    document.querySelector(".img6").style.padding = "0";
+    /* *************************************************************/
+    document.querySelector(".img1").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img2").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img4").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img5").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img3").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img6").style.transition = "0.3s ease-in-out";
+}
+
+function btn2() {
+    document.querySelector(".img2").style.opacity = "0.5";
+    document.querySelector(".img3").style.opacity = "0.5";
+    document.querySelector(".img5").style.opacity = "0.5";
+    document.querySelector(".img6").style.opacity = "0.5";
+    document.querySelector(".img1").style.opacity = "1.5";
+    document.querySelector(".img4").style.opacity = "1.5";
+    /* *************************************************************/
+    document.querySelector(".img2").style.padding = "20px";
+    document.querySelector(".img3").style.padding = "20px";
+    document.querySelector(".img5").style.padding = "20px";
+    document.querySelector(".img6").style.padding = "20px";
+    document.querySelector(".img1").style.padding = "0";
+    document.querySelector(".img4").style.padding = "0";
+    /* *************************************************************/
+    document.querySelector(".img2").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img3").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img5").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img6").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img1").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img4").style.transition = "0.3s ease-in-out";
+}
+
+function btn3() {
+    document.querySelector(".img1").style.opacity = "0.5";
+    document.querySelector(".img2").style.opacity = "0.5";
+    document.querySelector(".img4").style.opacity = "0.5";
+    document.querySelector(".img6").style.opacity = "0.5";
+    document.querySelector(".img2").style.opacity = "1.5";
+    document.querySelector(".img5").style.opacity = "1.5";
+    /* *************************************************************/
+    document.querySelector(".img1").style.padding = "20px";
+    document.querySelector(".img2").style.padding = "20px";
+    document.querySelector(".img4").style.padding = "20px";
+    document.querySelector(".img6").style.padding = "20px";
+    document.querySelector(".img2").style.padding = "0";
+    document.querySelector(".img5").style.padding = "0";
+    /* *************************************************************/
+    document.querySelector(".img1").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img2").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img4").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img6").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img2").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img5").style.transition = "0.3s ease-in-out";
+}
+
+function btn4() {
+    document.querySelector(".img1").style.opacity = "0.5";
+    document.querySelector(".img2").style.opacity = "0.5";
+    document.querySelector(".img4").style.opacity = "0.5";
+    document.querySelector(".img5").style.opacity = "0.5";
+    document.querySelector(".img3").style.opacity = "1.5";
+    document.querySelector(".img6").style.opacity = "1.5";
+    /* *************************************************************/
+    document.querySelector(".img1").style.padding = "20px";
+    document.querySelector(".img2").style.padding = "20px";
+    document.querySelector(".img4").style.padding = "20px";
+    document.querySelector(".img5").style.padding = "20px";
+    document.querySelector(".img3").style.padding = "0";
+    document.querySelector(".img6").style.padding = "0";
+    /* *************************************************************/
+    document.querySelector(".img1").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img2").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img4").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img5").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img3").style.transition = "0.3s ease-in-out";
+    document.querySelector(".img6").style.transition = "0.3s ease-in-out";
+}
